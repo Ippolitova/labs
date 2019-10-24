d5c7a343278c868653faf9d3890d639924704bc5
Ippolitova <56303006+Ippolitova@users.noreply.github.com>
2019-10-24 15:29:45 +0300

    Add files via upload

---
+++ TestBall.java
@@ -0,0 +1,8 @@
+public class TestBall {
+    public static void main(String[] args) {
+        Ball b1 = new Ball(90, 100);
+        System.out.println(b1);
+        b1.move(30, 15);
+        System.out.println(b1);
+    }
+}
