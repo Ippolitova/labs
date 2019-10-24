8a9bf5a7bd338489dff0d576510cd1d383ef3f59
Ippolitova <56303006+Ippolitova@users.noreply.github.com>
2019-10-24 15:20:56 +0300

    Add files via upload

---
+++ TestAuthor.java
@@ -0,0 +1,6 @@
+public class TestAuthor {
+    public static void main(String[] args) {
+        Author a1 = new Author("Irina", "Ipp.Ira@yandex.ru", 'F');
+        System.out.println(a1);
+    }
+}
